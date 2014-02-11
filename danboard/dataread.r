23e6566aefb55897006b06f1db00fa6cc96a7bdb
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-02-11 15:00:33 +0900

    header,row.names=TRUE

--- danboard/dataread.r
+++ danboard/dataread.r
@@ -1,27 +1,28 @@
 ##データ読み込みファイル
-setwd("C:/R/dev/apps/danboard/usr/lib/")
+#setwd("C:/R/dev/apps/danboard/usr/lib/")
 library("xlsx")
 
 #プロジェクトの指定
 #projn <- "JRER"
-projn <- "YAYOI14"
+#projn <- "YAYOI14"
+projn <- myfunc()
 
 #プロジェクトディレクトリの作成
 dir.create(paste("C:/R/dev/apps/datacheck/", projn, "/", sep=""), showWarnings = TRUE)
 dir.create(paste("C:/R/dev/apps/danboard/usr/out/", projn, "/", sep=""), showWarnings = TRUE)
 
 #Rシートを指定
-projs <- paste(projn,"_Master_r.xlsx",sep="")
+projs <- paste("C:/R/dev/apps/danboard/usr/lib/", projn,"_Master_r.xlsx",sep="")
 
 #読み込み  
-attlist  <- read.xlsx( projs, 1,  encoding="UTF-8")
-att      <- read.xlsx( projs, 2,  encoding="UTF-8")
-queslist <- read.xlsx( projs, 3,  encoding="UTF-8")
-ques     <- read.xlsx( projs, 4,  encoding="UTF-8")
-hearlist <- read.xlsx( projs, 5,  encoding="UTF-8")
-hear     <- read.xlsx( projs, 6,  encoding="UTF-8")
+attlist  <- read.xlsx( projs, 1,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+att      <- read.xlsx( projs, 2,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+queslist <- read.xlsx( projs, 3,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+ques     <- read.xlsx( projs, 4,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+hearlist <- read.xlsx( projs, 5,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+hear     <- read.xlsx( projs, 6,  encoding="UTF-8", header=TRUE, row.names=TRUE)
 
-aoilist  <- read.xlsx( projs, 9,  encoding="UTF-8")
-dur      <- read.xlsx( projs, 10, encoding="UTF-8", stringsAsFactors=FALSE)
-cou      <- read.xlsx( projs, 11, encoding="UTF-8", stringsAsFactors=FALSE)
-ttf      <- read.xlsx( projs, 12, encoding="UTF-8", stringsAsFactors=FALSE)
+aoilist  <- read.xlsx( projs, 9,  encoding="UTF-8", header=TRUE, row.names=TRUE)
+dur      <- read.xlsx( projs, 10, encoding="UTF-8", stringsAsFactors=FALSE, header=TRUE, row.names=TRUE)
+cou      <- read.xlsx( projs, 11, encoding="UTF-8", stringsAsFactors=FALSE, header=TRUE, row.names=TRUE)
+ttf      <- read.xlsx( projs, 12, encoding="UTF-8", stringsAsFactors=FALSE, header=TRUE, row.names=TRUE)
