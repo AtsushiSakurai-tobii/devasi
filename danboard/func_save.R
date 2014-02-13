60c44f8270c737558be1869c9c6c7df978938827
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-02-13 16:57:33 +0900

    update:SAVE=2 add

--- danboard/func_save.R
+++ danboard/func_save.R
@@ -4,10 +4,11 @@
 savefunc <- function (SAVE,SAVEFILE) {
   saveflg <- 0
   saveflg <- as.numeric(readline("Save 1:YES,0:NO>>Enter : "))
+  
   if(SAVE==1){
     if(saveflg>0){
       #保存ディレクトリを作成する
-      dir.create(paste("C:/R/dev/apps/datacheck/", projn, "_/", sep=""), showWarnings = TRUE)
+      dir.create(paste("C:/R/dev/apps/datacheck/", projn, "/", sep=""), showWarnings = TRUE)
       #保存ファイル名
       projc   <- paste("C:/R/dev/apps/datacheck/", projn, "/Export_", projn, "_AOIData_Checked.csv" , sep="")
       #ファイルを保存する
@@ -16,5 +17,22 @@ savefunc <- function (SAVE,SAVEFILE) {
     }else{
       print("保存しませんでした")
     } 
+  
+  }else if(SAVE==2){
+    
+    if(saveflg>0){
+      #保存ディレクトリを作成する
+      dir.create(paste("C:/R/dev/apps/danboard/usr/out/", projn, "/", sep=""), showWarnings = TRUE)
+      #保存ファイル名
+      projc   <- paste("C:/R/dev/apps/danboard/usr/out/", projn, "/Export_", projn, "_SPSS.csv" , sep="")
+      #ファイルを保存する
+      write.csv(SAVEFILE, file=projc, append=T, quote=T, col.names=T)
+      print("save")
+    }else{
+      print("保存しませんでした")
+    }
+    
+  }else{
+    print("ERROR:func_save")
   }
 }
