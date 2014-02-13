a5dfd6310a8233f9ccefb42011d3343a6398b34d
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-02-13 21:13:32 +0900

    work

--- danboard/func_subset.R
+++ danboard/func_subset.R
@@ -3,21 +3,57 @@
 #library("Deducer", lib.loc="C:/Users/asi/Documents/R/R-2.15.1/library")
 #deducer(cmd=NULL)
 
+#### work ####
+subsetfunc<-function(){
+  #aoilist
+  excelsheet  <- readline(paste("What is data sheet?(Excel:attlist,aoilist,...) : "))
+  #条件数
+  expn <- as.numeric(readline("Number of Expression : "))
+  subsetseachn <- ""
+  for(i in 1:expn){
+    #i<-1
+    #distance
+    subsetobjct  <- readline(paste("What is subset's object?(distance,no,...) [",i,"]:"))
+    #==
+    subsetlogic  <- readline(paste("What is subset's logical?(==,>...) [",i,"]:"))
+    #NEAR
+    subsetparam  <- readline(paste("What is subset's parameter? (NEAR,FAR,...)[",i,"]:"))
+    #aoilist$distance=="NEAR"
+    subsetseach  <- paste(excelsheet,"$",subsetobjct,subsetlogic,paste("'",subsetparam,"'",sep=""),sep="")
+    subsetseachn <- paste(subsetseachn,subsetseach,sep="")
+  }
+  print(subsetseachn)
+  
+  getsubset   <- eval(parse(text=subsetseachn))
+  subset.dat  <- subset(get(excelsheet), getsubset==TRUE)
+  print(head(subset.dat))
+  print(str(subset.dat))
+  print(dim(subset.dat))
+  return(subset.dat)
+}
+test<-subsetfunc()
+
+#### work ####
+
+
 #
 subexpression <- function(SHTDT){
-  for(i in 1:ncol(SHTDT)){
-    message(paste(i, "=>", colnames(SHTDT)[i]))
+  SHTDT<-"shtdt"
+  for(k in 1:ncol(get(SHTDT))){
+    message(paste(k, "=>", colnames(get(SHTDT))[k]))
   }
   
-  colmn  <- readline(paste("colmun : "))                          #distance
-  expre  <- readline(paste(SHTDT,"$",colnames(shtdt)[colmn],":")) #aoilist$distance
-  setexp <- paste(SHTDT,"$",colnames(shtdt)[colmn],expre, sep="") #aoilist$distance=="NEAR"
+  colmn  <- as.numeric(readline(paste("colmun : ")))                           #distance
+  expre  <- readline( paste(SHTDT, "$", colnames(shtdt)[colmn], ":" ,sep="") ) #aoilist$distance
+  setexp <- paste(SHTDT, "$", colnames(get(SHTDT))[colmn], expre, sep="")      #aoilist$distance=="NEAR"
   
+  return(setexp)
 }
-sheet <- c("attlist","att","queslist","ques","hearlist","hear","aoilist","dur","cou","ttf")
+
+
 subsetfunc <- function () {
   
-#  sheet <- c("attlist","att","queslist","ques","hearlist","hear","aoilist","dur","cou","ttf")
+  sheet <- c("attlist","att","queslist","ques","hearlist","hear","aoilist","dur","cou","ttf")
   
   for(i in 1:length(sheet)){
     message(paste(i, "=>", sheet[i]))
@@ -25,22 +61,23 @@ subsetfunc <- function () {
   
   #sheetから対象シートを選ぶ
   #shtno <- 7
-  print(shtno <- readline("sheet : "))
+  shtno <- as.numeric(readline("sheet : "))
   
   #選択したシートを変数にセットする
   shtdt <- get(sheet[shtno])
-
+  
   #検索条件数の指定
-  noexpn <- readline("Nomber of Expression : ")
+  noexpn <- as.numeric(readline("Number of Expression : "))
   logiop <- c("AND","OR","NOT")
   
   getexps <- ""
-  for(i in 1:noexp){
+  for(i in 1:noexpn){
     
+    #subexpression関数
     getexp     <- subexpression(shtdt)
     
-    for(h in 1:length(logiop)){
-      message(paste(h, "=>", logiop[i]))
+    for(j in 1:length(logiop)){
+      message(paste(h, "=>", logiop[j]))
     }
     
     getlogiop  <- readline(paste("logical operators : "))
@@ -48,6 +85,7 @@ subsetfunc <- function () {
     
     if(noexp-1==0)break
   }
-  setsubset  <- subset(shtd, getexps)
+  setsubset  <- subset("shtd", getexps)
+  return(setsubset)
 }
 subsetfunc()
