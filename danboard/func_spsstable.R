c14f6147d55a42c2b0a88a6ec2b3a175da581352
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-02-13 17:08:30 +0900

    update

--- danboard/func_spsstable.R
+++ danboard/func_spsstable.R
@@ -1,38 +1,69 @@
-##
 setwd("C:/R/dev/apps/danboard/")
 #SPSS用フォーマット
 #有効データの抽出
-(dur)
-(cou)
-(ttf)
+str(durget)
+str(couget)
+str(ttfget)
 
-SPSSTABLE<-function(AOIDATA, METRICSNAME){
-  library(reshape2) #melt関数を使用する
+SPSSTABLE<-function(AOIDATA, METRICSNAME, AOIMTX){
+  #AOIDATA    <-durget
+  #METRICSNAME<-"VD"
+  #AOIMTX     <-"duration"
   
-  mtcs <- AOIDATA
-  mtcs$ID2 <- t(attlist["att01",att$att01+2])
-  
-  #主キーを"ID2"とする
-  #ID2:DataCollect時に使用する被験者番号となる
-  mm <- melt(mtcs, id=c("ID2"))
+  mtcss <- data.frame(AOIDATA)
+  attid <- data.frame(t(attlist[1,att$att01+2]))
+  colnames(attid) <- "ID2"
+  mtcs  <- cbind(mtcss, ID=rownames(mtcss), attid)
+    
+  #主キーを"ID"とする
+  #ID:DataCollect時に使用する被験者番号となる
+  mm <- melt(mtcs, id=c("ID","ID2"))
   mm$Metrics <- METRICSNAME
   
-  name1 <- aoilist[mm$variable,]$name1
-  name2 <- aoilist[mm$variable,]$name2
-  
-  mm$AOI_name <- paste(name1,name2,sep="")
-  print(head(mm))
+  ############ work ############
+  AOI_name <- c()
+  for(i in 1:nrow(mm)){
+    #i<-3528
+    #name1sb <- subset(aoilist, aoilist$duration==as.matrix(mm$variable)[i])$name1
+    #name2sb <- subset(aoilist, aoilist$duration==as.matrix(mm$variable)[i])$name2
+    if(AOIMTX=="duration"){
+      name1sb <- subset(aoilist, aoilist$duration==as.matrix(mm$variable)[i])$name1
+      name2sb <- subset(aoilist, aoilist$duration==as.matrix(mm$variable)[i])$name2
+    }
+    if(AOIMTX=="count"){
+      name1sb <- subset(aoilist, aoilist$count==as.matrix(mm$variable)[i])$name1
+      name2sb <- subset(aoilist, aoilist$count==as.matrix(mm$variable)[i])$name2
+    }
+    if(AOIMTX=="ttf"){
+      name1sb <- subset(aoilist, aoilist$ttf==as.matrix(mm$variable)[i])$name1
+      name2sb <- subset(aoilist, aoilist$ttf==as.matrix(mm$variable)[i])$name2
+    }
+    AOI_name[i] <- paste(name1sb, name2sb, sep="")
+  }
+  ############ work ############
+
+  print(length(AOI_name))
   print(dim(mm))
-  return(mm)
+  tbl <- cbind(mm, AOI_name)
+  print(dim(tbl))
+  print(head(tbl))
+  print(tail(tbl))
+  return(tbl)
 }
-spssdur<-SPSSTABLE(dur, "VD")
-spsscou<-SPSSTABLE(cou, "VC")
-spssttf<-SPSSTABLE(ttf, "TTF")
+library(reshape2) #melt関数を使用する
+spssdur<-SPSSTABLE(durget, "VD",  "duration")
+spsscou<-SPSSTABLE(couget, "VC",  "count")
+spssttf<-SPSSTABLE(ttfget, "TTF", "ttf")
 
 comb<-rbind(spssdur,spsscou,spssttf)
-head(comb)
-dim(comb)
-str(comb)
+spss.ds <- data.frame(ID=comb$ID, ID2=comb$ID2, AOI_name=comb$AOI_name, AOI_RMetrics=comb$variable, Metrics=comb$Metrics, quantity=comb$value)
+spss.ds.ord <- spss.ds[order(spss.ds$ID,spss.ds$AOI_name),]
+rownames(spss.ds.ord) <- NULL
 
-a<-data.frame(ID=comb$ID2, AOI_name=comb$AOI_name, AOI_R=comb$variable, Metrics=comb$Metrics, quantity=comb$value)
-b<-a[order(a$att01),]
+#SPSS仕様
+#quantity:NA->""とする
+q<-ifelse(spss.ds.ord, is.na(spss.ds.ord$quantity), "", spss.ds.ord$quantity)
+
+
+#savefunc("保存フラグ","保存するDataSet")
+savefunc(2,spss.ds.ord)
