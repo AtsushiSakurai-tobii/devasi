10e9e5e23e85949485003d4df7b329658953264c
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-03-03 18:08:53 +0900

    delet #

--- danboard/check_aoidata_plot.R
+++ danboard/check_aoidata_plot.R
@@ -1,12 +1,9 @@
-setwd(paste("C:/R/dev/apps/datacheck/", projn, sep=""))
+setwd(paste("C:/R/dev/apps/datacheck/", projectname, sep=""))
 #plotCheck
 (durget)
 (couget)
 (ttfget)
 
-#plot(durget, type="l")
-#boxplot(durget)
-#
 dursum  <- apply(durget, 2, sum , na.rm = TRUE)
 cousum  <- apply(couget, 2, sum , na.rm = TRUE)
 ttfsum  <- apply(ttfget, 2, sum , na.rm = TRUE)
@@ -17,32 +14,13 @@ durSd   <- apply(durget, 2, sd  , na.rm = TRUE)
 couSd   <- apply(couget, 2, sd  , na.rm = TRUE)
 ttfSd   <- apply(ttfget, 2, sd  , na.rm = TRUE)
 
-#op<-par(mfrow = c(3,1))
-#plot(dursum,col="red")
-#plot(cousum,col="blue")
-#plot(ttfsum,col="green")
-#par(op)
-#
-#op<-par(mfrow = c(3,1))
-#barplot(dursum,col="red")
-#barplot(cousum,col="blue")
-#barplot(ttfsum,col="green")
-#par(op)
-#
-#op<-par(mfrow = c(3,1))
-#hist(dursum, breaks = 300, col = "red")
-#hist(cousum, breaks = 300, col = "blue")
-#hist(ttfsum, breaks = 300, col = "green")
-#par(op)
-
 #Histgram
 #各メトリクス全体のヒストグラム
-png(paste("Export_", projn, "_chkplot_hist_allmetrics.png",sep=""), width=1280, height=1024)
+png(paste("Export_", projectname, "_chkplot_hist_allmetrics.png",sep=""), width=1280, height=1024)
 par(mfrow = c(3,1))
 hist(durget, col="red"  , breaks = 300)
 hist(couget, col="blue" , breaks = 300)
 hist(ttfget, col="green", breaks = 300)
-#hist(durget, breaks = "Scott", col="blue")
 par(op)
 dev.off()
 
@@ -55,26 +33,18 @@ simple.hist.and.boxplot <- function(x) {
   boxplot(melt(x)$value, horizontal = TRUE, las = 0, ylim = range(c(0, max(x, na.rm=TRUE))))
   par(op)
 }
-png(paste("Export_", projn, "_chkplot_hist-box_dur.png",sep=""), width=1280, height=1024)
+png(paste("Export_", projectname, "_chkplot_hist-box_dur.png",sep=""), width=1280, height=1024)
 simple.hist.and.boxplot(durget)
 dev.off()
-png(paste("Export_", projn, "_chkplot_hist-box_cou.png",sep=""), width=1280, height=1024)
+png(paste("Export_", projectname, "_chkplot_hist-box_cou.png",sep=""), width=1280, height=1024)
 simple.hist.and.boxplot(couget)
 dev.off()
-png(paste("Export_", projn, "_chkplot_hist-box_ttf.png",sep=""), width=1280, height=1024)
+png(paste("Export_", projectname, "_chkplot_hist-box_ttf.png",sep=""), width=1280, height=1024)
 simple.hist.and.boxplot(ttfget)
 dev.off()
 
-#Hist(log)
-#hist(log(durget), col="red"  , breaks = 300)
-#r <- hist(durget)
-#r <- hist(durget, col="red"  , breaks = 300)
-#r <- hist(couget, col="blue" , breaks = 300)
-#r <- hist(ttfget, col="green", breaks = 300)
-#plot(r$breaks[-1], r$counts, log='xy', type='h')
-
 #qqnorm:データの正規性確認
-png(paste("Export_", projn, "_chkplot_qqnorm_allmetrics.png",sep=""), width=1280, height=1024)
+png(paste("Export_", projectname, "_chkplot_qqnorm_allmetrics.png",sep=""), width=1280, height=1024)
 op<-par(mfrow = c(3,1))
 qqnorm( durget )
 qqline( durget, lwd=2, col="red" )
@@ -84,35 +54,3 @@ qqnorm( ttfget )
 qqline( ttfget, lwd=2, col="red" )
 par(op)
 dev.off()
-
-#op<-par(mfrow = c(3,1))
-#aod <- barplot(durMean, ylab = "Average of Duration(s)", xlab = "AOI")
-#arrows(aod, durMean - durSd, aod, durMean + durSd, angle = 90, length = 0.1)
-#arrows(aod, durMean + durSd, aod, durMean - durSd, angle = 90, length = 0.1)
-#
-#aoc <- barplot(couMean, ylab = "Average of Count(n)"   , xlab = "AOI")
-#arrows(aoc, couMean - couSd, aoc, couMean + couSd, angle = 90, length = 0.1)
-#arrows(aoc, couMean + couSd, aoc, couMean - couSd, angle = 90, length = 0.1)
-#
-#aot <- barplot(ttfMean, ylab = "Average of TTF(s)"     , xlab = "AOI")
-#arrows(aot, ttfMean - ttfSd, aot, ttfMean + ttfSd, angle = 90, length = 0.1)
-#arrows(aot, ttfMean + ttfSd, aot, ttfMean - ttfSd, angle = 90, length = 0.1)
-#par(op)
-
-#散布図行列
-#library("psych", lib.loc="C:/Users/asi/Documents/R/R-2.15.1/library")
-#pairs.panels(durget[,1:ncol(durget)], scale=TRUE)
-#pairs(durget[,1:ncol(durget)], lower.panel=panel.cor)
-
-#外れ値
-#library("corrplot", lib.loc="C:/Users/asi/Documents/R/R-2.15.1/library")
-#corrplot(log(durget))
-#
-#source("http://aoki2.si.gunma-u.ac.jp/R/src/SG.R", encoding="euc-jp")
-#SG(durget)
-#
-##http://stat.biopapyrus.net/statistic/r-outliers.html
-#library(outliers)
-#cochran.test(apply(durget, 2, var, na.rm=TRUE), 1:length(durget))
-#grubbs.test(data.frame(durget))
-setwd("C:/R/dev/apps/danboard/")
