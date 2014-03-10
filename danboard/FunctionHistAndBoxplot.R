00c6793fa82a69d8e29d06ce1722d87d67ff163a
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-03-10 17:21:51 +0900

    New make plots Function(Histgram & Boxplot)

---
+++ danboard/FunctionHistAndBoxplot.R
@@ -0,0 +1,9 @@
+HistAndBoxplot <- function(x,MAINNAME) {
+  if(max(x, na.rm=TRUE)>0){
+    op<-par(mfrow = c(2,1))
+    hist(x, col = "blue", breaks = 10, xlim = c(0, max(x, na.rm=TRUE)), main=MAINNAME )
+    rug(x)
+    boxplot(melt(x)$value, horizontal = TRUE, las = 0, ylim = range(c(0, max(x, na.rm=TRUE))))
+    par(op)
+  }
+}
