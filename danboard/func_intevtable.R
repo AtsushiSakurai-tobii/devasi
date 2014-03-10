2aabfc703abd5ca28dbf83f3fbeccb3d1563ca4f
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-03-10 17:25:05 +0900

    Function DATATABLE update

--- danboard/func_intevtable.R
+++ danboard/func_intevtable.R
@@ -1,7 +1,7 @@
 #InterviewData(intev)をMeltする
 #
 #PRO.SPSS <- spss.table
-#SELECTED <- (rec <- intev[,1:4])
+#SELECTED <- rec <- intev[,1:4]
 
 DATATABLE <- function(PRO.SPSS, SELECTED){
   #meltする
@@ -11,10 +11,10 @@ DATATABLE <- function(PRO.SPSS, SELECTED){
   
   #PRO.SPSSのSubNo&aoigroupに対して被験者情報(selected.melt)を取得する
   for(i in 1:nrow(PRO.SPSS)){
-    
     #subsとaoigroup1を検索条件とする(固定条件)
     ############################################################################
-    (meltdata.get   <- selected.melt[c(PRO.SPSS$subs[i],PRO.SPSS$aoigroup[i]),])
+    #(meltdata.get   <- selected.melt[c(PRO.SPSS$subs[i], PRO.SPSS$aoigroup1[i]),])
+    (meltdata.get   <- subset(selected.melt, subset=(Var1==PRO.SPSS$subs[i] & Var2==PRO.SPSS$aoigroup1[i])))
     ############################################################################
     
     (meltdata.table <- rbind(meltdata.table, meltdata.get))
