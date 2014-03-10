b5b1d281dc6e3339d90f2984e99b62af33e9bd64
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-03-10 17:30:12 +0900

    New Output R Plots and new statics

---
+++ danboard/morimilk-Rmain_.R
@@ -0,0 +1,13 @@
+dp<-FunctionPercent(durget)
+cp<-FunctionPercent(couget)
+tp<-FunctionPercent(ttfget)
+
+FunctionGroupOfObject("dp","duration","aoilist","aoigroup1")
+FunctionGroupOfObject("cp","count","aoilist","aoigroup1")
+FunctionGroupOfObject("tp","ttf","aoilist","aoigroup1")
+
+FunctionGroupOfAttribute("dp","duration","att","att04")
+FunctionGroupOfAttribute("cp","count","att","att04")
+FunctionGroupOfAttribute("tp","ttf","att","att04")
+
+FunctionGroupOfAttribute("dp","duration","intev","intev001")
