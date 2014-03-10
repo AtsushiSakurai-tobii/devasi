97eaf1f14e6b92102146777ec48c2a14fcfb5802
AtsushiSakurai-tobii <atsushi.sakurai@tobii.co.jp>
2014-03-10 17:25:41 +0900

    add dir.reate

--- danboard/func_my.R
+++ danboard/func_my.R
@@ -4,4 +4,11 @@
 myfunc <- function () {
   Str <- readline("Enter : ")
   #as.numeric(unlist(strsplit(Str, ",")))
+  
+  #プロジェクトディレクトリの作成
+  dir.create(paste("C:/R/dev/apps/datacheck/", Str, "/", sep=""), showWarnings = TRUE)
+  dir.create(paste("C:/R/dev/apps/danboard/usr/out/", Str, "/", sep=""), showWarnings = TRUE)
+  
+  return(Str)
+  
 }
